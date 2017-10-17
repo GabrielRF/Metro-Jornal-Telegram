b15799cb2190901b50c64b0460959eb87972e7c2
GabrielRF <github@gabrf.com>
2017-10-17 20:44:16 -0200

    Sendind message

--- routine.py
+++ routine.py
@@ -1,25 +1,32 @@
 from datetime import datetime
+import configparser
 import requests
+import sys
+import telebot
+
+arg = sys.argv[1]
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('bot.conf')
+
+token = config[arg]['TOKEN']
+city = config[arg]['CITY']
 
 now = datetime.now()
 today = now.strftime('%Y%m%d')
 hoje = now.strftime('%d/%m/%Y')
-city = 'Brasilia'
 
-cover = 'https://www2.metrojornal.com.br/pdf/assets/capas/{}_{}_Capa.jpg'.format(today, city)
-pdf = 'https://www2.metrojornal.com.br/pdf/assets/pdfs/{}_{}.pdf'.format(today, city)
+cover = 'https://www2.metrojornal.com.br/pdf/assets/capas/{}_{}_Capa.jpg'.format(today, arg)
+pdf = 'https://www2.metrojornal.com.br/pdf/assets/pdfs/{}_{}.pdf'.format(today, arg)
 
 response = requests.get(cover)
-print(response.status_code)
+bot = telebot.TeleBot(token)
 
 if 200 != response.status_code:
     print('fail')
 else:
-    print(cover)
-    print(pdf)
-
-    message = ('Metro Jornal<a href="{}">.</a>'.format(cover) +
-        '\n<b>{}</b> {}'.format(city, hoje) +
-        '\n<a href="{}">PDF</a>'.format(pdf))
-
-    print(message)
+    message = ('<b>{}</b><a href="{}">.</a>'.format(city, cover) +
+        '\n{}'.format(hoje) +
+        '\n<a href="{}">Arquivo PDF</a>'.format(pdf))
+    bot.send_message('@MetroJornal', message, parse_mode='HTML')
