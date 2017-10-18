6a31ab42e0b4505f4f1505e353f5c0f9cca2b39f
GabrielRF <github@gabrf.com>
2017-10-18 10:36:50 -0200

    Retry if not available

--- routine.py
+++ routine.py
@@ -3,6 +3,7 @@ import configparser
 import requests
 import sys
 import telebot
+import time
 
 arg = sys.argv[1]
 
@@ -12,21 +13,31 @@ config.read('bot.conf')
 
 token = config[arg]['TOKEN']
 city = config[arg]['CITY']
-
 now = datetime.now()
 today = now.strftime('%Y%m%d')
 hoje = now.strftime('%d/%m/%Y')
 
 cover = 'https://www2.metrojornal.com.br/pdf/assets/capas/{}_{}_Capa.jpg'.format(today, arg)
 pdf = 'https://www2.metrojornal.com.br/pdf/assets/pdfs/{}_{}.pdf'.format(today, arg)
-
-response = requests.get(cover)
 bot = telebot.TeleBot(token)
 
-if 200 != response.status_code:
-    print('fail')
-else:
-    message = ('<b>{}</b><a href="{}">.</a>'.format(city, cover) +
-        '\n{}'.format(hoje) +
-        '\n<a href="{}">Arquivo PDF</a>'.format(pdf))
-    bot.send_message('@MetroJornal', message, parse_mode='HTML')
+def get_ed():
+    response = requests.get(cover)
+    if 200 != response.status_code:
+        print('fail')
+        return 0
+    else:
+        message = ('\U0001F4F0 <b>{}</b><a href="{}">.</a> #{}'.format(city, cover, city.replace(' ','')) +
+            '\n{}'.format(hoje) +
+            '\n<a href="{}">Arquivo PDF</a>'.format(pdf))
+        bot.send_message('9083329', message, parse_mode='HTML')
+        return 1
+
+if __name__ == '__main__':
+    sent = get_ed()
+    while sent == 0:
+        time.sleep(900)
+        sent = get_ed()
+        
+
+
