81996ffb14a4904421cd65114cd6d681a1ce4f91
GabrielRF <github@gabrf.com>
2019-08-14 08:13:22 -0300

    Improved fix when not available

--- routine.py
+++ routine.py
@@ -26,41 +26,36 @@ cover = 'https://www2.metrojornal.com.br/pdf/assets/capas/{}_{}_Capa.jpg'.format
 pdf = 'https://www.readmetro.com/pt/brazil/{}/{}/1/'.format(arg.lower(), todayclean)
 bot = telebot.TeleBot(token)
 
-def get_ed():
+def get_ed(cover, pdf):
     message = ('<a href="{}">\U0001F4F0</a> #{}'.format(cover, city.replace(' ','')) +
         '\n{}'.format(hoje) +
         '\n<a href="{}">Clique aqui para ler</a>'.format(pdf))
+    print(message)
     for i in dest.split(','):
         bot.send_message(i, message, parse_mode='HTML')
-    return 1
 
 def get_img(url):
     print(url)
     response = requests.get(url)
     if '404!' in response.text:
-        preview = True
+        return False, None
     html = BeautifulSoup(response.content, 'html.parser')
     img = html.find('meta', {'property': 'og:image'})
     if not img:
         img = html.find('meta', {'name': 'og:image'})
     try:
         img = img['content']
-        preview = False
         if 'http:' not in img and 'https:' not in img:
             img = 'http:' + img
     except TypeError:
         img = ''
-        preview = True
+        return False, None
     print(img)
-    return preview, img
-
+    return True, img
 
 if __name__ == '__main__':
     preview, cover = get_img(url)
-    attemp = 0
-    sent = 0
-    while sent == 0 and preview == False and attemp < 5:
-        time.sleep(900)
-        attemp = attemp + 1
-        sent = get_ed()
+    print(preview)
+    if preview == True:
+        get_ed(cover, pdf)
 
